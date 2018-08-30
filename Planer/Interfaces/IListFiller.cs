88d242574ffdb70ca59fa9e752d944ed7578c95e
kblaszczyk3002 <42248910+kblaszczyk3002@users.noreply.github.com>
2018-08-30 17:55:08 +0200

    Create IListFiller.cs

---
+++ Planer/Interfaces/IListFiller.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planer.Interfaces
+{
+    public interface IListFiller
+    {
+        void FillList(int records);
+    }
+}
