7c3e6ae64470661d820a02b0b6fcba5ae032bd69
kblaszczyk3002 <42248910+kblaszczyk3002@users.noreply.github.com>
2018-08-30 17:52:48 +0200

    Create Provinces.cs

---
+++ Planer/Helpers/Provinces.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planer.Helpers
+{
+    public class Provinces
+    {
+        public enum Wojewodztwa
+        {
+            Dolnośląskie,
+            Kujawsko_pomorskie,
+            Lubelskie,
+            Lubuskie,
+            Łódzkie,
+            Małopolskie,
+            Mazowieckie,
+            Opolskie,
+            Podkarpackie,
+            Podlaskie,
+            Pomorskie,
+            Śląskie,
+            Świętokrzyskie,
+            Warmińsko_mazurskie,
+            Wielkopolskie,
+            Zachodniopomorskie
+
+        }
+    }
+}
