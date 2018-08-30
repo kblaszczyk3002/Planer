bfbdf6d0f94ad3786229b54c8451eef6b4cac08e
kblaszczyk3002 <42248910+kblaszczyk3002@users.noreply.github.com>
2018-08-30 17:53:07 +0200

    Add files via upload

---
+++ Planer/Helpers/WindowHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Planer.Helpers
+{
+    public class WindowHelpers
+    {
+        public virtual void Btn_previousClick(TabControl tcSample)
+        {
+            int newIndex = tcSample.SelectedIndex - 1;
+
+            if (newIndex < 0)
+                newIndex = tcSample.Items.Count - 1;
+            tcSample.SelectedIndex = newIndex;
+        }
+
+        public virtual void Btn_nextClick(TabControl tcSample)
+        {
+            int newIndex = tcSample.SelectedIndex + 1;
+
+            if (newIndex >= tcSample.Items.Count)
+                newIndex = 0;
+            tcSample.SelectedIndex = newIndex;
+        }
+    }
+}
